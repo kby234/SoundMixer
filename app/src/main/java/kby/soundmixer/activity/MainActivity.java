9579364f9254a4539a50692954d850b0730b97b2
kby234 <46399034+kby234@users.noreply.github.com>
2019-01-05 22:45:35 +0900

    Initial commit

---
+++ app/src/main/java/kby/soundmixer/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package kby.soundmixer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kby.soundmixer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
